73ef3cbd6a9752ff5b79b64f81f9e0eefd9835a0
uplShreya <shreya.saha@utplco.com>
2022-03-31 01:08:04 +0530

    project table part started

--- src/components/projects/ProjectHeader.tsx
+++ src/components/projects/ProjectHeader.tsx
@@ -6,91 +6,84 @@ import style from "../../components/projects/Project.module.css";
 
 function ProjectHeader() {
   return (
-    <div style={{ padding: "1em" }}>
-      <div style={{ fontSize: "20px", fontFamily: "sans-serif" }}>
+    <div style={{ padding: "2em" }}>
+      <div
+        style={{ fontSize: "25px", fontFamily: "sans-serif", height: "4em" }}
+      >
         <p>Projects</p>
       </div>
-      <div className={style.projectheaderleftright}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-          }}
-        >
-          <div className={style.projectheaderleft}>
-            <div>
-              <p style={{ color: "#8a8a8a" }}>Filter</p>
+      <div style={{ width: "109%" }}>
+        <div className={style.projectheaderborder}>
+          <div className={style.projectheaderleftright}>
+            <div className={style.projectheaderleft}>
+              <div>
+                <p style={{ color: "#8a8a8a" }}>Filter</p>
+              </div>
+              <div
+                style={{
+                  borderRight: "1px dotted gray",
+                  marginTop: "5px",
+                  height: "3em",
+                }}
+              ></div>
+              <div>
+                <p style={{ color: "#595959" }}>Active</p>
+              </div>
+              <div
+                style={{
+                  borderRight: "1px dotted gray",
+                  marginTop: "5px",
+                  height: "3em",
+                }}
+              ></div>
+              <div>
+                <p style={{ color: "#595959" }}>Client</p>
+              </div>
+              <div
+                style={{
+                  borderRight: "1px dotted gray",
+                  marginTop: "5px",
+                  height: "3em",
+                }}
+              ></div>
+              <div>
+                <p style={{ color: "#595959" }}>Access</p>
+              </div>
+              <div
+                style={{
+                  borderRight: "1px dotted gray",
+                  marginTop: "5px",
+                  height: "3em",
+                }}
+              ></div>
+              <div>
+                <p style={{ color: "#595959" }}>Billing</p>
+              </div>
+              <div
+                style={{
+                  borderRight: "1px dotted gray",
+                  marginTop: "5px",
+                  height: "3em",
+                }}
+              ></div>
             </div>
-            <div
-              style={{
-                borderRight: "1px dotted gray",
-                marginTop: "5px",
-                height: "3em",
-              }}
-            ></div>
-            <div>
-              <p style={{ color: "#595959" }}>Active</p>
-            </div>
-            <div
-              style={{
-                borderRight: "1px dotted gray",
-                marginTop: "5px",
-                height: "3em",
-              }}
-            ></div>
-            <div>
-              <p style={{ color: "#595959" }}>Client</p>
-            </div>
-            <div
-              style={{
-                borderRight: "1px dotted gray",
-                marginTop: "5px",
-                height: "3em",
-              }}
-            ></div>
-            <div>
-              <p style={{ color: "#595959" }}>Access</p>
-            </div>
-            <div
-              style={{
-                borderRight: "1px dotted gray",
-                marginTop: "5px",
-                height: "3em",
-              }}
-            ></div>
-            <div>
-              <p style={{ color: "#595959" }}>Billing</p>
-            </div>
-            <div
-              style={{
-                borderRight: "1px dotted gray",
-                marginTop: "5px",
-                height: "3em",
-              }}
-            ></div>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              width: "60%",
-              justifyContent: "space-between",
-            }}
-          >
-            <div>
-              {/* <p>hi</p> */}
-              <Image src="/image/search.svg" height={55} width={20} />
-            </div>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              {/* <p>hi</p> */}
-              <TextFieldInput
-                type="search"
-                placeholder="Project name"
-                textnewclass={style.input}
-              />
-            </div>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              {/* <p>hi</p> */}
-              <Button variant="contained">Apply Filter</Button>{" "}
+            {/* left part end */}
+            {/* <div style={{ width: "70%" }}> */}
+            <div className={style.projectheaderright}>
+              <div>
+                <Image src="/image/search.svg" height={55} width={20} />
+              </div>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <TextFieldInput
+                  type="search"
+                  placeholder="Project name"
+                  textnewclass={style.input}
+                />
+              </div>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <Button variant="contained">Apply Filter</Button>
+              </div>
+              {/* </div> */}
             </div>
           </div>
         </div>
--- src/container/projectscontainer/ProjectPage.tsx
+++ src/container/projectscontainer/ProjectPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ProjectHeader from "../../components/projects/ProjectHeader";
+import ProjectTable from "../../components/projects/projectTable";
 
 function ProjectPage() {
   return (
     <div style={{}}>
       <ProjectHeader />
+      <ProjectTable />
     </div>
   );
 }
