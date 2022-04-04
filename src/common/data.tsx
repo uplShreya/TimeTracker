abff1ce2a9284d39c1944a5e51accf57b192eb92
uplShreya <shreya.saha@utplco.com>
2022-04-04 23:37:37 +0530

    routing done

---
+++ src/common/data.tsx
@@ -0,0 +1,8 @@
+export const sideNav = [
+  { name: "TIME TRACKER", icon: "/image/timetrackericon.svg" },
+  { name: "CALENDER", icon: "/image/calendar.svg" },
+  { name: "DASHBOARD", icon: "/image/dashboard.svg" },
+  { name: "REPORTS", icon: "/image/reports.svg" },
+  { name: "PROJECTS", icon: "/image/projects.svg" },
+  { name: "TEAM", icon: "/image/teams.svg" },
+];
