cacf8cbb0e7b16dfad775de8aadba2bb213873ed
uplRajdwip <rajdwip.dey@utplco.com>
2022-04-06 14:32:02 +0530

    daterangepicker update

--- src/common/UiComponent/commonDatetime/commonDatetime.tsx
+++ src/common/UiComponent/commonDatetime/commonDatetime.tsx
@@ -33,11 +33,11 @@ const CommonDate = (props: any) => {
                     />
                 </Stack>
             </div>}
-            
+
             {props.date2 && <div className={styles.date2}>
                 <div className={styles.dateinput_fild}>
-                <Image src='/image/calendar.svg' width={20} height={30} className={styles.date2I_mage} />
-                <LocalizationProvider dateAdapter={AdapterDateFns} className={styles.data_fild}>
+                    <Image src='/image/calendar.svg' width={20} height={30} className={styles.date2I_mage} />
+                    {/* <LocalizationProvider dateAdapter={AdapterDateFns} className={styles.data_fild}>
                     <DateRangePicker
                         label="Advanced keyboard"
                         value={value}
@@ -58,7 +58,24 @@ const CommonDate = (props: any) => {
                             </React.Fragment>
                         )}
                     />
-                </LocalizationProvider>
+                </LocalizationProvider> */}
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DateRangePicker
+                            startText="Check-in"
+                            endText="Check-out"
+                            value={value}
+                            onChange={(newValue) => {
+                                setValue(newValue);
+                            }}
+                            renderInput={(startProps, endProps) => (
+                                <React.Fragment>
+                                    <TextField {...startProps} />
+                                    <Box sx={{ mx: 2 }}> - </Box>
+                                    <TextField {...endProps} />
+                                </React.Fragment>
+                            )}
+                        />
+                    </LocalizationProvider>
                 </div>
             </div>}
         </div>
