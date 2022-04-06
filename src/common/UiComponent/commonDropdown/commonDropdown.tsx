cd9238ed2f19e1bc346ffa550d61eb640a2390c0
uplRajdwip <rajdwip.dey@utplco.com>
2022-04-06 11:34:35 +0530

    dropdown changes

--- src/common/UiComponent/commonDropdown/commonDropdown.tsx
+++ src/common/UiComponent/commonDropdown/commonDropdown.tsx
@@ -2,32 +2,36 @@ import { ArrowDropDown } from '@material-ui/icons'
 import React from 'react'
 import styles from '../ui.module.css'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { FormControl, FormHelperText, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 const CommonDropdown = (props: any) => {
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setAge(event.target.value);
+    };
     return (
         <div>
 
             {props.dropdown &&
-                <div>
-                    <select name="cars" id="cars" className={styles.dropDown}>
-                        <option className={styles.dropDown_option} value={props.firstvalue}>{props.firstName} </option>
-                        <option className={styles.dropDown_option} value={props.secoundvalue}>{props.secoundNmae}</option>
-                        
-                    </select>
-                    {/* <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                <div className={styles.dropDown}>
+
+                    <FormControl fullWidth sx={{ m: 1, minWidth: 110, height: 28, justifyContent: 'center' }}>
+                        {/* <InputLabel id="demo-simple-select-label">Age</InputLabel> */}
                         <Select
                             labelId="demo-simple-select-label"
                             id="demo-simple-select"
-                            // value={age}
-                            // label="Age"
+                            value={age}
+                            // label={props.firstvalue}
                             onChange={handleChange}
                         >
-                            <MenuItem value={10}>Ten</MenuItem>
-                            <MenuItem value={20}>Twenty</MenuItem>
-                            <MenuItem value={30}>Thirty</MenuItem>
+                            <MenuItem value={props.firstvalue}>{props.firstName}</MenuItem>
+                            <MenuItem value={props.secoundvalue}>{props.secoundNmae}</MenuItem>
                         </Select>
-                    </FormControl> */}
+                    </FormControl>
+                    {/* <select name="cars" id="cars" className={styles.dropDown}>
+                        <option className={styles.dropDown_option} value={props.firstvalue}>{props.firstName} </option>
+                        <option className={styles.dropDown_option} value={props.secoundvalue}>{props.secoundNmae}</option>
+                    </select> */}
                 </div>}
 
         </div>
