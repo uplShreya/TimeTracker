62692b1285615d880ae51ae0cdabcff4c73f1132
uplRajdwip <rajdwip.dey@utplco.com>
2022-04-06 18:22:51 +0530

    repoarts update

---
+++ pages/reports/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReportsContainer from '../../src/container/reports/Reports'
+const Reports = () => {
+    return (
+        <div>
+            <ReportsContainer />
+        </div>
+    )
+}
+
+export default Reports
---
+++ src/components/reports/Reports.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReportsComponent = () => {
+  return (
+    <div>Reports</div>
+  )
+}
+
+export default ReportsComponent
