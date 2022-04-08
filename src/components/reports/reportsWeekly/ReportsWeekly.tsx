78d9d9031240ee23caf0a1ee487eeca479fe8c90
uplRajdwip <rajdwip.dey@utplco.com>
2022-04-08 14:19:45 +0530

    build problem solved

--- src/components/reports/reportsWeekly/ReportsWeekly.tsx
+++ src/components/reports/reportsWeekly/ReportsWeekly.tsx
@@ -375,8 +375,8 @@ const ReportsWeekly = () => {
             </tr>
           </thead>
 
-          {rows.map((row) => (
-            <tr style={{ border: "1px solid #C6D2D9", height: "3.5em" }}>
+          {rows.map((row: any) => (
+            <tr key={row} style={{ border: "1px solid #C6D2D9", height: "3.5em" }}>
               <td>
               <div style={{ paddingLeft: "1.5em" }}>
                 <div
